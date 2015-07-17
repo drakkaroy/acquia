0cfdb74e0d6b21904b83b87c042649c4d25d7390
Mario Vega <mvegacr@gmail.com>
2015-07-17 07:54:05 -0600

    Update mymodule.js
    Added FB login

--- sites/all/modules/custom/mymodule/mymodule.js
+++ sites/all/modules/custom/mymodule/mymodule.js
@@ -17,7 +17,52 @@
 
     	//console.log();
     	$(Drupal.theme('myModuleTheme', new Date())).insertAfter($('h1'));
-    }
+			console.log('Calling to initFB');
+			this.initFacebook();
+    },
+		initFacebook: function(){
+			$.ajaxSetup({ cache: true });
+			$.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+				FB.init({
+					appId: '929603380416764',
+					xfbml: true,
+					version: 'v2.4' // or v2.0, v2.1, v2.0
+				});
+				FB.getLoginStatus(Drupal.behaviors.mymodule.updateStatusCallback);
+			});
+
+			$('.fb-logout').click(function(){
+				if (typeof(FB)!== 'undefined'){
+					FB.api("/me/permissions","DELETE",function(response){
+					    Drupal.behaviors.mymodule.updateStatusCallback(response); //gives true on app delete success 
+					});
+				}
+			});
+
+
+		},
+		updateStatusCallback:function(response){
+			console.log('updateStatusCallback');
+			console.log(response);
+			if(response.status === 'connected'){
+				$('.fb-login-button').hide();
+				$('.userStatus').text('User connected. FB ID=' + response.authResponse.userID);
+				$('.fb-logout').show();
+			}else{
+				$('.userStatus').text('User not conected. Please login.');
+				$('.fb-login-button').show();
+				$('.fb-logout').hide();
+
+			}
+			FB.Event.subscribe('auth.statusChange', function(response) {
+				console.log('auth.statusChange');
+				Drupal.behaviors.mymodule.updateStatusCallback(response);
+			});
+		},
+		afterFBlogin: function(response){
+			console.log('user logged');
+			console.log(response);
+		}
   };
 
 
